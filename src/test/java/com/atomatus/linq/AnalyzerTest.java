944dddac8483d00a60179e408adbf66ea5e15c78
Carlos Matos <chcmatos@hotmail.com>
2020-11-19 20:04:14 -0300

    Implemented Analyzer Test.

---
+++ src/test/java/com/atomatus/linq/AnalyzerTest.java
@@ -0,0 +1,17 @@
+package com.atomatus.linq;
+
+import junit.framework.TestCase;
+
+public class AnalyzerTest extends TestCase {
+
+    @SuppressWarnings("SpellCheckingInspection")
+    public void testAnalyzer() {
+        //Example, analyzing how many patients have obesity.
+        // obs.: real data of patients with respiratory conditions between years 2018 and 2019 in Brazil.
+        try(Analyzer a = Analyzer.load("https://raw.githubusercontent.com/chcmatos/nanodegree_py_analyze_srag/main/doc/influd18_limpo-final.csv")) {
+            int count = a.get("OBESIDADE").asInteger().count(e -> e == 1 /*0 - NAO TEM OBESIDADE, 1 - TEM OBESIDADE, 4 - NAO AVALIADO*/);
+            assertEquals(1466, count);
+            System.out.printf("Were found %d patients having obesity.", count);
+        }
+    }
+}
